9d6295589f3629c67f0cadebf99ee3c3eebb7467
Matt Kam <matt.ph.kam@gmail.com>
2020-04-22 00:26:00 -0700

    added some practice for redux-saga

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -5,7 +5,7 @@ import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
   UNSAFE_componentWillUpdate(){
-    console.log('[OrderSummary] WillUpdate');
+    // console.log('[OrderSummary] WillUpdate');
   }
   
   render () {
